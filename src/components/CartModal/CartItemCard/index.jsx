dc2230776cfa94b87720d18cf38713114b1b6c36
Josenilson Farias <fariaslwork@gmail.com>
2023-08-10 15:28:08 -0300

    styles: modal

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -1,11 +1,12 @@
 import { MdDelete } from "react-icons/md";
+import style from "./style.module.scss"
 
 export const CartItemCard = ({ product, len, setLen }) => {
    return (
-      <li>
+      <li className={style.li}>
          <div>
             <img src={product.img} alt={product.name} />
-            <h3>{product.name}</h3>
+            <h3 className="title three black">{product.name}</h3>
          </div>
          <button aria-label="delete" title="Remover item">
             <MdDelete size={21} />
